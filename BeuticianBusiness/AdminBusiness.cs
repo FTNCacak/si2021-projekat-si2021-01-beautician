e4101e85092c99ec9483fea65563a387d80f2697
DESKTOP-2P0OGR3 <ivanavukovicimd@gmail.com>
2022-01-09 00:38:10 +0100

    Kreirana klasa na biznis sloju i interfejs na share sloju.

---
+++ BeuticianBusiness/AdminBusiness.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Models;
+using Shared.Interfaces;
+
+namespace BeuticianBusiness
+{
+    public class AdminBusiness
+    {
+
+        private readonly IAdminRepository adminRepository;
+
+        public AdminBusiness(IAdminRepository adminRepository)
+        {
+            this.adminRepository = adminRepository;
+        }
+
+        public List<Admin> GetAdmins()
+        {
+            return this.adminRepository.GetAdmins();
+        }
+
+
+        public bool AdminLoginValidation(string username, string password)
+        {
+            List<Admin> admins = GetAdmins();
+
+
+            foreach (Admin admin in admins)
+            {
+                if (admin.Username == username && admin.Password == password)
+                    return true;
+
+            }
+
+            return false;
+
+
+        }
+    }
+}
