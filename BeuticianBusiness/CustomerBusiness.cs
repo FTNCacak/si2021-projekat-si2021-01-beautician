42010be0f10aba9cb2c046c47544ab2a012ea566
DESKTOP-2P0OGR3 <ivanavukovicimd@gmail.com>
2022-01-11 23:14:50 +0100

    Zavrsila sam sve klase i interfejse na biznis sloju.

---
+++ BeuticianBusiness/CustomerBusiness.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Models;
+using Shared.Interfaces;
+
+namespace BeuticianBusiness
+{
+    public class CustomerBusiness : ICustomerBusiness
+    {
+        private readonly ICustomerRepository customerRepository;
+
+        public CustomerBusiness(ICustomerRepository customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public void InsertCustomer(Customer customer)
+        {
+
+            customerRepository.InsertCustomer(customer);
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            return customerRepository.GetCustomers();
+        }
+
+        public bool CustomerExists(string email)
+        {
+            List<Customer> customers = GetCustomers();
+
+            if (customers.Count == 0)
+                return false;
+
+            foreach (var customer in customers)
+            {
+                if (customer.Email == email)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public bool LoginValidation(string email, string password)
+        {
+            List<Customer> customers = GetCustomers();
+
+            foreach (var customer in customers)
+            {
+                if (customer.Email == email && customer.Password == password)
+                    return true;
+            }
+
+            return false;
+        }
+
+
+    }
+}
---
+++ BeuticianBusiness/OrderBusiness.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Shared.Models;
+using Shared.Interfaces;
+
+namespace BeuticianBusiness
+{
+    public class OrderBusiness
+    {
+        private readonly IOrderRepository orderRepository;
+
+        public OrderBusiness(IOrderRepository orderRepository)
+        {
+            this.orderRepository = orderRepository;
+        }
+        public void InsertOrder(Order order)
+        {
+            orderRepository.InsertOrder(order);
+        }
+
+        public List<Order> GetOrders()
+        {
+
+            return orderRepository.GetOrders();
+        }
+
+        public Order GetSpecificOrder(int CustomerId, DateTime date)
+        {
+            Order order = new Order();
+
+            List<Order> orders = GetOrders();
+
+            foreach (Order item in orders)
+            {
+                if (item.CustomerId == CustomerId && item.Date == date)
+                    order = item;
+            }
+
+
+            return order;
+        }
+
+
+
+
+    }
+}
